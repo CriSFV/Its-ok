b112a3b1e455c098ade830484ef1e904da39f370
CriSFV <cristina.f.valverde@gmail.com>
2021-12-27 17:05:12 +0100

    everything its ok

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+const img = document.querySelector('.js_img');
+const title = document.querySelector('.js_title');
+const text = document.querySelector('.js_paragraph');
+
+function handleShowImg() {
+  img.setAttribute('src', './assets/images/meme1.jpg');
+  text.innerHTML = "I'm not going to cry";
+}
+
+title.addEventListener('click', handleShowImg);
+
+function handleChangeImg() {
+  img.setAttribute('src', './assets/images/meme2.jpg');
+}
+
+img.addEventListener('click', handleChangeImg);
